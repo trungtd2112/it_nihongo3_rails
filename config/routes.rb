dfe46034195b796d841822b36b789510c9c79a2d
Ubuntu <ubuntu@ip-172-31-67-106.ec2.internal>
2020-11-22 04:20:11 +0000

    Processing homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get '/', to: 'static_pages#home', as: 'home'
   devise_for :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
